63914682e55d40aaa0370c6aadb2ef808685a780
Anjaom <eric@webworkbench.net>
2023-10-24 13:34:48 -0600

    hibernate one to many with annotations

--- database/src/main/java/org/perscholas/database/CreateOrderExample.java
+++ database/src/main/java/org/perscholas/database/CreateOrderExample.java
@@ -5,8 +5,10 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 import org.perscholas.database.dao.OrderDAO;
+import org.perscholas.database.entity.Customer;
 import org.perscholas.database.entity.Order;
 
 public class CreateOrderExample {
@@ -38,12 +40,27 @@ public class CreateOrderExample {
 		
 	}
 	
+	public void queryOrder() {
+		Order o = orderDao.findById(10100);
+		
+		Customer c = o.getCustomer();
+		
+		System.out.println("Order " + o.getId() + " was made by customer " + c.getCustomerName() + " on " + o.getOrderDate());
+		
+		// all the customers orders
+		List<Order> orders = c.getOrders();
+		for ( Order order : orders ) { 
+			System.out.println("Order " + order.getId());
+		}
+	}
+	
 	
 	public static void main(String[] args) {
 		CreateOrderExample x = new CreateOrderExample();
 		try {
-			x.createOrder();
-		} catch (ParseException e) {
+			//x.createOrder();
+			x.queryOrder();
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
--- database/src/main/java/org/perscholas/database/entity/Order.java
+++ database/src/main/java/org/perscholas/database/entity/Order.java
@@ -4,9 +4,12 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -20,8 +23,16 @@ public class Order {
     @Column(name = "id")
 	private Integer id;
     
-    @Column(name = "customer_id")
+	// this variable becomes read only because we have set
+	// insertable and updateble to false.
+	// we need to do this because we now have a @ManyToOne annotation
+	// that is working on the customer_id field also.
+    @Column(name = "customer_id", insertable = false, updatable = false)
     private Integer customerId;
+    
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
     
     @Column(name = "order_date")
     @Temporal(TemporalType.DATE)
@@ -95,6 +106,14 @@ public class Order {
 
 	public void setComments(String comments) {
 		this.comments = comments;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
 	}
     
     
