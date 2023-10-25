9593bf9677c0ba23e557c68c149345ec638e3530
Anjaom <eric@webworkbench.net>
2023-10-25 14:21:43 -0600

    order details homework

--- database/src/main/java/org/perscholas/database/CreateOrderDetails.java
+++ database/src/main/java/org/perscholas/database/CreateOrderDetails.java
@@ -19,13 +19,18 @@ public class CreateOrderDetails {
 	
 	public void createOrderDetails() {
 		// ask the user to enter a product name
+		
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter a product name to add to your order:");
 		String productName = scanner.nextLine();
 		
 		// looks up a product by a productName
+		// TODO make the product name work with like .. so I Can enter a partial product name
+		// whill have to google getting like to work with hibernate
+		// and get back a list of "Ford" products
 		List<Product> products = productDAO.findByName(productName);
 		
+		// this will display the products on the screen
 		if ( products.size() == 0 ) {
 			System.out.println("You did not enter a valid proper product name");
 			System.exit(0);
@@ -44,17 +49,20 @@ public class CreateOrderDetails {
 		// the goal is to insert a record into the order details table
 		Product p = productDAO.findById(productId);
 		
+		// TODO this should anotehr check here to make sure that they product loaded is a product
+		
+		// TODO add the ability to ask the user for an order number
 		// ask the user what order number they want to add the product too
 		Order o = orderDAO.findById(10100);
 		
-		// if the product is already part of the odrer do not add it again
-		for ( OrderDetail orderDetail : o.getOrderDetails() )  {
-			if ( orderDetail.getProduct().getId() == productId) {
-				// the product is already part of the order
-				System.out.println("The product " + p.getProductName() + " is already part of the order. can not add again");
-				System.exit(1);
-			}
-		}
+		// TODO
+		// if the order is null then print a message saying invalid order number
+		
+
+		// TODO .... if the product is already part of the order details - 
+		// print a message saying you can not add a duplicate product
+		// its its not part of the order then add it to the order
+		
 		
 		OrderDetail od = new OrderDetail();
 		od.setProduct(p);
