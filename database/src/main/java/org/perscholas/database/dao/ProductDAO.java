b238f35422fc85fdc6bedc52d2868cf010e74aa5
Anjaom <eric@webworkbench.net>
2023-10-27 12:57:44 -0600

    more database examples and homework review

--- database/src/main/java/org/perscholas/database/dao/ProductDAO.java
+++ database/src/main/java/org/perscholas/database/dao/ProductDAO.java
@@ -13,19 +13,19 @@ import org.perscholas.database.entity.Order;
 import org.perscholas.database.entity.Product;
 
 public class ProductDAO {
+	
+	private SessionFactory factory = new Configuration().configure().buildSessionFactory();
+	private Session session = factory.openSession();
+	
 
 	public Product findById(Integer id) {
-		SessionFactory factory = new Configuration().configure().buildSessionFactory();
-		Session session = factory.openSession();
-
 		String hql = "FROM Product p WHERE p.id = :id"; // Example of HQL to get all records of user class
 
 		TypedQuery<Product> query = session.createQuery(hql, Product.class);
 		query.setParameter("id", id);
 
 		try {
-			Product result = query.getSingleResult();
-			
+			Product result = query.getSingleResult();	
 			return result;
 		} catch (NoResultException nre) {
 			return null;
@@ -33,28 +33,20 @@ public class ProductDAO {
 	}
 	
 	public List<Product> findByName(String name) {
-		SessionFactory factory = new Configuration().configure().buildSessionFactory();
-		Session session = factory.openSession();
-
 		String hql = "FROM Product p WHERE p.productName like :name"; // Example of HQL to get all records of user class
 
 		TypedQuery<Product> query = session.createQuery(hql, Product.class);
 		query.setParameter("name", "%" + name + "%");
 		List<Product> result = query.getResultList();
-		session.close();
 		
 		return result;
 	}
 	
 	
 	public void save(Product product) {
-		SessionFactory factory = new Configuration().configure().buildSessionFactory();
-		Session session = factory.openSession();
-
 		Transaction t = session.beginTransaction();
 		session.saveOrUpdate(product);
 		t.commit();
-		session.close();
 	}
 	
 }
