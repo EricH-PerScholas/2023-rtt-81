936e85172a5c4e40b144b1236cfce154210ad225
Anjaom <eric@webworkbench.net>
2023-10-30 13:03:55 -0600

    added parameterized tests

--- jnuit-examples/src/test/java/perscholas/testing/CalculatorTest.java
+++ jnuit-examples/src/test/java/perscholas/testing/CalculatorTest.java
@@ -2,19 +2,38 @@ package perscholas.testing;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CalculatorTest {
 
-	@Test
-	public void additionTest() {
+    @ParameterizedTest
+    @CsvSource({
+            "5, 5, 10",
+            "10, 10, 20",
+            "5.5, 5.5, 11.0"
+    })
+	public void additionTest(double a, double b, double answer) {
 		Calculator c = new Calculator();
-		double expected = 10.0;
 		
-		double actual = c.add(5.0, 5.0);
+		double actual = c.add(a, b);
 		
-		Assertions.assertEquals(expected,actual);
+		Assertions.assertEquals(answer, actual);
+	}
+    
+    
+    @ParameterizedTest
+    @CsvSource({
+            "5, 5, 12",
+            "10, 10, 20.5",
+            "5.5, 5.5, 12.0"
+    })
+	public void additionErrorTest(double a, double b, double answer) {
+		Calculator c = new Calculator();
 		
-		Assertions.assertEquals(15.0, c.add(7.5,7.5));
+		double actual = c.add(a, b);
+		
+		Assertions.assertNotEquals(answer, actual);
 	}
 	
 	@Test
